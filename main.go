2a780eaf01f9ad6a7f32c099314c19af01f1220b
Andre Silva <andreswebs@pm.me>
2024-04-12 15:59:48 -0300

    debug

--- main.go
+++ main.go
@@ -72,6 +72,8 @@ func init() {
 func handler(ctx context.Context, req Request) (Response, error) {
 	headerValue, ok := req.Headers[headerKey]
 
+	slog.Debug("debug headers", slog.String("type", "application.inspect"), env, slog.Any("headers", req.Headers))
+
 	if ok && headerValue == secret {
 		slog.Info("allowed", slog.String("type", "application"), env, slog.Bool("h", ok), slog.String("target", req.MethodArn))
 		return generatePolicy(principalID, "Allow", req.MethodArn), nil
